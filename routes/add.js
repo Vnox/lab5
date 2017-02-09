07edd1ab9a5afb2e385692613be02aba51e299a7
Shuangli Zhou <shz103@ucsd.edu>
2017-02-09 10:48:34 -0800

    finished lab5 sort of

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,15 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var name = req.query.name;
+	var description = req.query.description;
+
+	var newFriend = {
+		name:name,
+		description:description,
+		imageURL:"http://dummy"
+	}
+
+	data.friends.push(newFriend);
+	res.redirect('/');
  }
